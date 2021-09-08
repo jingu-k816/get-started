bd97aa630f5c16d4cd39df394537092f3de15378
Jingu Kang <jinguk0816@gmail.com>
2021-09-08 01:18:17 -0400

    change file directory and add layout to the project also make a temporary nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
 import styles from '../styles/Home.module.css'
-import About from '../src/presentation/components/about'
-import Index from '../src/presentation/components/index'
-import Items from '../src/presentation/components/items'
-import NavBar from '../src/presentation/navbar'
+import Layout from './src/presentation/components/Layout'
 
 
-export default function Home() {
+export default function Home({}) {
   return (
     <div className={styles.container}>
-      <About />
-      <Index />
-      <Items />
-      <NavBar />
+      hello world
     </div>
   )
 }
