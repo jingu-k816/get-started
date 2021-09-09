807208fbfeb053d674788a0a2d3eee722deeae02
Jingu Kang <jinguk0816@gmail.com>
2021-09-09 18:48:57 -0400

    add type to variables and remove console log

--- src/infrastructure/api/jsonCall.ts
+++ src/infrastructure/api/jsonCall.ts
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
 const apiCallFunc = async() => {
-  const token = '1k5sbhafeq3stb477djbommo9z2vrwj40x1zymsa'
+  const token: string = '1k5sbhafeq3stb477djbommo9z2vrwj40x1zymsa'
   const apiCall = await axios.get('https://api.json-generator.com/templates/NM0ZuloCu1TZ/data', {headers: {"Authorization" : `Bearer ${token}`}})
-    .then(res => {
-      console.log("Call request sucess!");
-      return res.data;
-    })
+    .then(res => res.data)
     .catch(err => console.error(err));
 
   return apiCall;
