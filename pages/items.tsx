e6f1804aea181cefbfb133c8cf0ba9a49768b336
Jingu Kang <jinguk0816@gmail.com>
2021-09-10 13:00:31 -0400

    add seo to items page

--- pages/items.tsx
+++ pages/items.tsx
@@ -5,7 +5,13 @@ import { NextSeo } from 'next-seo';
 const Items: React.FC = () => {
   const SEO = {
     title: 'Items Page',
-    description: 'This is an Items page to see items from JSON data'
+    description: 'This is an Items page to see items from JSON data',
+    openGraph: {
+      type: "website",
+      url: "localhost:3000",
+      description: "This is another items page's description",
+      site_name: "Get Started"
+    }
   }
   return(
     <>
