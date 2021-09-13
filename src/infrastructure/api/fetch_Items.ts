355a4c646690b7c4c22dc2fafd7647d78c99a929
Jingu Kang <jinguk0816@gmail.com>
2021-09-13 16:50:05 -0400

    change file name to follow naming convention and add error for failure to fetch data

--- src/infrastructure/api/fetch_Items.ts
+++ src/infrastructure/api/fetch_Items.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const apiCallFunc = async() => {
+const fetchItems = async() => {
   const token: string = '1k5sbhafeq3stb477djbommo9z2vrwj40x1zymsa'
   const apiCall = await axios.get('https://api.json-generator.com/templates/NM0ZuloCu1TZ/data', {headers: {"Authorization" : `Bearer ${token}`}})
     .then(res => res.data)
-    .catch(err => console.error(err));
+    .catch(err => alert(`Unknown error occured! \n${err}`));
 
   return apiCall;
 }
 
-export default apiCallFunc;
+export default fetchItems;
