c4bc03894b65d2d0b5247e77027094b380fb64b2
Jingu Kang <jinguk0816@gmail.com>
2021-09-14 14:06:30 -0400

    change type for the onClickHandler

--- src/presentation/components/Button.tsx
+++ src/presentation/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Button} from '@material-ui/core';
 
 interface LoadButtonProps {
-  onChange: React.MouseEventHandler<HTMLButtonElement>,
+  onChange: () => void,
   name: string
 }
 
